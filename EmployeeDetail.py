57927441890fc761747fc280cea7d2099bca94c8
vinayanotad <chinnunotad2001@gmail.com>
2023-02-09 16:20:59 +0530

    9/2/23

--- EmployeeDetail.py
+++ EmployeeDetail.py
@@ -2,9 +2,9 @@ def getdata():
     count=0
     while(count<10):
         count=count+1
-        print("Enter the details of Employee\n")
+        print("\nEnter the details of Employee")
         name = input("Enter your name:") 
         age = int(input("Enter your age:"))  
-        salary = float(input("Enter your salary: ")) 
+        salary = float(input("Enter your salary:  ")) 
 
 getdata()
