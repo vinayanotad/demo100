080d146e4438c5a7d3a061507740181d7b691a66
vinayanotad <chinnunotad2001@gmail.com>
2023-02-21 18:21:36 +0530

    21/2

--- 11.py
+++ 11.py
@@ -1,12 +1,64 @@
-p,q,r =5,10,15
-print(q,r) 
-p,q=q,p
-print (p,q) 
+# p,q,r =5,10,15
+# print(q,r) 
+# p,q=q,p
+# print (p,q) 
 
-a,b,c = 5,10,7
-b,c,a = a+1, b+2, c-1
-print(a,b,c) 
+# a,b,c = 5,10,7
+# b,c,a = a+1, b+2, c-1
+# print(a,b,c) 
 
-x = 10
-y,y = x+2,x+5
-print(y)
+# x = 10
+# y,y = x+2,x+5
+# print(y)
+
+# n=11
+# if(n%2==0):
+#     print("Given no is Even",n)
+# else:
+#     print("Given no is Odd",n)
+
+
+# print("\t\tKEONICS\t\tvinaya")
+
+# a,b,c=5,3,3
+# print(a+b*c)
+
+
+# p,q,r =5,10,15
+# print(q, r) 
+# p,q=q,p
+# print (p,q) 
+
+
+# n=1
+# count=0
+# for i in range(1,2):
+#     if(n%i==0):
+#         count=count+1
+#     if(count==2):
+#         print("prime",n) 
+#     else:
+#         print("not prime",n)   
+
+
+
+print("\\")
+print("\\Hello\\")
+print("\' Sainik School Amaravathinagar\'")  #Black Slash
+print("\'Praveen M Jigajini\'")  #single quote
+print("\"Sainik School\"")  #double quotes
+print("\a\a\a\aComputer programming is Fun")  #Ascii bells
+print("\b Python")   #Ascii backslash
+print("\f Python Programming")   #Ascii form feed
+print("\n WELCOME TO PYTHON TUTORIAl")  #new line
+print("\r VAST FEATURES")  #Carriage return 
+print("\t Hiii \t How \t are \t you")   #Hortinzontal tab
+print("\v\v Sequences")  #Vertical tab
+print("\x41")
+print("\x42")   #16 bit Hex values
+print("\110")
+print("\111")
+print("\120")  #\ooo Octal value
+
+
+
