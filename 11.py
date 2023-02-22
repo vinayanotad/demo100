669da3c7ae7b8d174c7db5c0831bc435592df45c
vinayanotad <chinnunotad2001@gmail.com>
2023-02-22 17:30:19 +0530

    22/02/23

--- 11.py
+++ 11.py
@@ -42,23 +42,47 @@
 
 
 
-print("\\")
-print("\\Hello\\")
-print("\' Sainik School Amaravathinagar\'")  #Black Slash
-print("\'Praveen M Jigajini\'")  #single quote
-print("\"Sainik School\"")  #double quotes
-print("\a\a\a\aComputer programming is Fun")  #Ascii bells
-print("\b Python")   #Ascii backslash
-print("\f Python Programming")   #Ascii form feed
-print("\n WELCOME TO PYTHON TUTORIAl")  #new line
-print("\r VAST FEATURES")  #Carriage return 
-print("\t Hiii \t How \t are \t you")   #Hortinzontal tab
-print("\v\v Sequences")  #Vertical tab
-print("\x41")
-print("\x42")   #16 bit Hex values
-print("\110")
-print("\111")
-print("\120")  #\ooo Octal value
+# print("\\")
+# print("\\Hello\\")
+# print("\' Sainik School Amaravathinagar\'")  #Black Slash
+# print("\'Praveen M Jigajini\'")  #single quote
+# print("\"Sainik School\"")  #double quotes
+# print("\a\a\a\aComputer programming is Fun")  #Ascii bells
+# print("\b Python")   #Ascii backslash
+# print("\f Python Programming")   #Ascii form feed
+# print("\n WELCOME TO PYTHON TUTORIAl")  #new line
+# print("\r VAST FEATURES")  #Carriage return 
+# print("\t Hiii \t How \t are \t you")   #Hortinzontal tab
+# print("\v\v Sequences")  #Vertical tab
+# print("\x41")
+# print("\x42")   #16 bit Hex values
+# print("\110")
+# print("\111")
+# print("\120")  #\ooo Octal value
+
+# print(oct(17))
+# print(bin(5))
+
+# val=0xA0C
+# print("value",val)
+
+# a=0O7
+# print(a)
+
+# c=0b1100
+# print(c)
+
+# print(oct(7))
+# print(hex(2572))
+# print(bin(12))
+
+x='True'
+print(x)
+
+x=None
+print(x)
+
+
 
 
 
---
+++ code_1.py
@@ -0,0 +1,16 @@
+def getdata(name,age,salary):
+    print("Enter the details\n")
+    name = input("Enter your name:") 
+    age = int(input("Enter your age:"))  
+    salary = float(input("Enter your salary: ")) 
+
+
+def config():
+    for (e==0,e<10,e++)
+    getdata(name,age,salary)
+    display()
+
+def display():
+    e=employee("name",name,"age",age,"salary",salary)
+
+config()
